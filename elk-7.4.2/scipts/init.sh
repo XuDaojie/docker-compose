2b0e24c189ec339f2aaacf0d75e210a532fb9be7
XuDaojie <xudaojieg@gmail.com>
2024-03-31 00:24:33 +0800

    update elk7

---
+++ elk-7.4.2/scipts/init.sh
@@ -0,0 +1,16 @@
+# #!/bin/bash
+
+
+# #获取脚本的路径
+# scriptPath=$(cd $(dirname $0);pwd)
+# #获取脚本的父路径
+# homePath=dir=$(dirname $scriptPath)
+# echo "scriptPath: ${scriptPath}"
+# echo "homePath: ${homePath}"
+
+# cd '/home/xdj/Workspace/docker-compose'
+# echo "$(pwd)"
+# mkdir "data"
+# chmod g+rwx "data"
+# # chgrp 0 data
+
